abd1cb9f59ec31fc96ceacfc31dfdd1f78026a76
123 2333 <dany021270@gmail.com>
2021-01-26 20:12:52 +0300

    тут нет 6

--- Practici/sosci/Program.cs
+++ Practici/sosci/Program.cs
@@ -87,46 +87,193 @@ namespace sosci
             //        $" Матч: {item.Match} ,Голы: {item.Goals}"  );
             //}
             #endregion
-            #region(Тут третье)
-            var grouppedByTeams = from t in Teams
-                                  group t.Title by t.Id into t
-                                  select new
-                                  {
-                                      Team = t.Key
-                                  };
-            var workerInTeams = from t in grouppedByTeams
-                                join w in Workers on t.Team equals w.TeamId
-                                select new
-                                 {
-                                     Team = t.Team,
-                                     WorkerName = w.FirstName,
-                                     WorkerSurname = w.LastName,
-                                     Post = w.Position
-                                 };
-        
-            var playersInTeams = from t in workerInTeams
+            #region(Тут четвертое)
+            //var grouppedByTeams = from t in Teams
+            //                      group t.Title by t.Id into t
+            //                      select new
+            //                      {
+            //                          Team = t.Key
+            //                      };
+            //var workerInTeams = from t in grouppedByTeams
+            //                    join w in Workers on t.Team equals w.TeamId
+            //                    select new
+            //                     {
+            //                         Team = t.Team,
+            //                         WorkerName = w.FirstName,
+            //                         WorkerSurname = w.LastName,
+            //                         Post = w.Position
+            //                     };
+
+            //var playersInTeams = from t in workerInTeams
+            //                     join p in Players on t.Team equals p.TeamId
+            //                     select new
+            //                     {
+            //                         Team = t.Team,
+            //                         WorkerName = t.WorkerName,
+            //                         WorkerSurname = t.WorkerSurname,
+            //                         Post = t.Post,
+            //                         PlayerName = p.FirstName,
+            //                         PlayerLastname = p.LastName
+
+            //                     };
+            //foreach (var item in playersInTeams)
+            //{
+            //    Console.WriteLine($"Команда - {item.Team}, " +
+            //        $" Имя сотрудника - {item.WorkerName} " +
+            //        $"Фамилия сотрудника - {item.WorkerSurname} " +
+            //        $"Должность сотрудника - {item.Post} " +
+            //        $"Имя игрока - {item.PlayerName} " +
+            //        $"Фамилия игрока - {item.PlayerLastname} " );
+            //}
+
+
+            #endregion
+            #region(тут пятое)
+            //var grouppedReferee = from r in Referees
+            //                      group r.FirstName by r.Id into r
+            //                      select new
+            //                      {
+            //                          Reeferee = r.Key
+            //                      };
+            //var gefereesInMatches = from r in grouppedReferee
+            //                        join m in MatchesAndReferees on r.Reeferee equals m.RefereeId
+            //                        select new
+            //                        {
+            //                            Reeferee = r.Reeferee,
+            //                            Match = m.MatchId
+            //                        };
+            //var refereeAndMatches = from r in gefereesInMatches
+            //                        join m in Matches on r.Match equals m.Id
+            //                        select new
+            //                        {
+            //                            Referee = r.Reeferee,
+            //                            Match = m.Id,
+            //                            Matchdate = m.MatchDate,
+            //                            LIVE = m.IsFinished,
+            //                            Stadium = m.StadionId,
+            //                            HomeTeam = m.HomeTeamId,
+            //                            GuestTeam = m.GuestTeamId
+            //                        };
+            //var matchesInStadiums = from m in refereeAndMatches
+            //                        join s in Stadiums on m.Stadium equals s.Id
+            //                        select new
+            //                        {
+            //                            HomeTeam = m.HomeTeam,
+            //                            GuestTeam = m.GuestTeam,
+            //                            Referee = m.Referee,
+            //                            Match = m.Match,
+            //                            Matchdate = m.Matchdate,
+            //                            LIVE = m.LIVE,
+            //                            Stadium = s.Name
+
+            //                        };
+            //var homePlayersInMatches = from m in matchesInStadiums
+            //                           join t in Teams on m.HomeTeam equals t.Id
+            //                           select new
+            //                           {
+            //                               HomeTeam = t.Title,
+            //                               GuestTeam = m.GuestTeam,
+            //                               Referee = m.Referee,
+            //                               Match = m.Match,
+            //                               Matchdate = m.Matchdate,
+            //                               LIVE = m.LIVE,
+            //                               Stadium = m.Stadium,
+
+            //                           };
+            //var guestPlayersInMatches = from m in homePlayersInMatches
+            //                            join t in Teams on m.GuestTeam equals t.Id
+            //                            select new
+            //                            {
+            //                                HomeTeam = m.HomeTeam,
+            //                                GuestTeam = t.Title,
+            //                                Referee = m.Referee,
+            //                                Match = m.Match,
+            //                                Matchdate = m.Matchdate,
+            //                                LIVE = m.LIVE,
+            //                                Stadium = m.Stadium,
+
+            //                            };
+            //var refereesInMatches = from m in guestPlayersInMatches
+            //                        join r in Referees on m.Referee equals r.Id
+            //                        select new
+            //                        {
+            //                            HomeTeam = m.HomeTeam,
+            //                            GuestTeam = m.GuestTeam,
+            //                            RefereeName = r.FirstName,
+            //                            RefereeLastName = r.LastName,
+            //                            Match = m.Match,
+            //                            Matchdate = m.Matchdate,
+            //                            LIVE = m.LIVE,
+            //                            Stadium = m.Stadium,
+            //                        };
+
+            //foreach (var item in refereesInMatches)
+            //{
+            //    Console.WriteLine(
+            //                      $"Судья: {item.RefereeName} {item.RefereeLastName}\n" +
+            //                      $"ID Матча: {item.Match}\n" +
+            //                      $"Дата проведения матча: {item.Matchdate}\n" +
+            //                      $"Стадион проведения: {item.Stadium}\n" +
+            //                      $"Домашняя команда: {item.HomeTeam},\n" +
+            //                      $"Команда-Гость: {item.GuestTeam},\n" +
+            //                      $"Закончился ли матч:{item.LIVE}\n");
+            //}
+            #endregion
+            #region(тут шестое)
+            var groupedTeams = from t in Teams
+                               group t.Title by t.Id into t
+                               select new
+                               {
+                                   Team = t.Key
+                               };
+            
+
+            var playersInTeams = from t in groupedTeams
                                  join p in Players on t.Team equals p.TeamId
                                  select new
                                  {
                                      Team = t.Team,
-                                     WorkerName = t.WorkerName,
-                                     WorkerSurname = t.WorkerSurname,
-                                     Post = t.Post,
+                                     Position = p.Position,
                                      PlayerName = p.FirstName,
-                                     PlayerLastname = p.LastName
+                                     PlayerSurname = p.LastName
+                                 };            
+            var teamsInPlayers = from p in playersInTeams
+                                 join t in Teams on p.Team equals t.Id
+                                 select new
+                                 {
+                                     Team = t.Title,
+                                     Position = p.Position,
+                                     PlayerName = p.PlayerName,
+                                     PlayerSurname = p.PlayerSurname,
+                                     Team2 = Teams
 
                                  };
-            foreach (var item in playersInTeams)
-            {
-                Console.WriteLine($"Команда - {item.Team}, " +
-                    $" Имя сотрудника - {item.WorkerName} " +
-                    $"Фамилия сотрудника - {item.WorkerSurname} " +
-                    $"Должность сотрудника - {item.Post} " +
-                    $"Имя игрока - {item.PlayerName} " +
-                    $"Фамилия игрока - {item.PlayerLastname} " );
-            }
+            var groupedPositions = from t in teamsInPlayers
+                                   group teamsInPlayers by t.Position into sas
+                                   select new
+                                   {
+                                       sas.Key
+                                   };
+
+            //foreach (var pos in groupedPositions)
+            //{
+            //    Console.WriteLine($"{pos.Key}");
+            //    foreach (var Team2 in pos)
+            //    {
+            //        Console.WriteLine(Team2.);
+            //    }
+            //}
 
 
+
+            //foreach (var item in groupedPositions)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //foreach (var item in groupedPositions)
+            //{
+            //   
+            //}
             #endregion
         }
     }
